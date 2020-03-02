98244c1efa7023dbbc44de1ce61cbb8423b28e84
Apoorv Singh <Apoorv-Lagwal@live.nmit.ac.nz>
2020-03-03 10:00:10 +1300

    hello

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 5;
+var sum = a + b;
+console.log(sum);
+
+var a = 10, b = 5; 
+var sub = a - b;
+console.log(sub);
+
+var a = 10, b = 5, div = a / b;
+console.log(div);
+
+var a = 5, b = 10, perc = a % b;
+console.log(perc);
